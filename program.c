02347cd77bcbee376a6e5c0ac6c10b3cc88b09ad
taha <taharca12@gmail.com>
2022-06-24 20:37:36 +0100

    تعريف بيانات من نوع عدد بطريقة التعريف بعد الوضع

--- program.c
+++ program.c
@@ -1,9 +1,7 @@
-// this is a simple program that excute a sum operation
-
 #include <stdio.h>
 int main(){
-int num1=10,num2=10;
-int sum = num1+num2;
-
-printf("%i" ,sum);
+    int x;
+    x=1;
+    printf("%i" ,x);
+    return 0;
 };
