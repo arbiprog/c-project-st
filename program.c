ee967525dc0d183fae41d91e6c68f07c9d998bde
taha <taharca12@gmail.com>
2022-06-24 19:48:30 +0100

    تعليق سطر و متعدد السطور

--- program.c
+++ program.c
@@ -1,4 +1,7 @@
+// this is the c program 
+/* this is a simple program that show a simple message 
+"مرحبا أنت في تقدم" */
 #include <stdio.h>
-int main(){
-    printf("مرحبا");
+int main (){
+    printf("مرحبا \n أنت في تقدم");
 };
