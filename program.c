a4b5704fbfb9799040cc569f91768abc236c8b01
taha <taharca12@gmail.com>
2022-06-25 16:03:16 +0100

    switch statment

--- program.c
+++ program.c
@@ -1,46 +1,14 @@
-// if , else if , else statments
+// switch statments
 
 #include <stdio.h>
 int main(){
-// int x=3;
-// int y=4;
-// if(x>0 && y>0){
-//     printf("x and y are greater than 0");
-// };
+int x=2;
+switch (x){
+    case 1:printf("1");break;
+    case 2:printf("2");break;
 
-// if
-// int x=3;
-// int y=41;
-// if(x>10 || y>10){
-//     printf("x and y are greater than 0");
-// };
+    default:printf("0");
 
-
-// if else
-// int x=3;
-// int y=4;
-// if(x>10 && y>10){
-//     printf("x and y are greater than 0");
-// }else{
-//     printf("NOT !");
-// };
-
-
-// if, else if, else ,
-// int x=3;
-// int y=4;
-// if(x>10 && y>10){
-//     printf("x and y are greater than 10");
-// }else if(x>10 || y>10){
-//     printf("x or y are greater than 10");
-// }else{
-//     printf("no variable is greater than 10 ");
-// };
-
-
-// short hand of f
-int x= 10;
-(x>5) ? printf("x greater than 5") : printf("x not greater than 5");
-
+};
 return 0;
 };
