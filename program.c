63137c955a265665cec2f10ed7b602e5cb2b7856
taha <taharca12@gmail.com>
2022-06-25 17:11:44 +0100

    do while

--- program.c
+++ program.c
@@ -1,16 +1,13 @@
 // switch statments
-
 #include <stdio.h>
 int main(){
 
 
-int i=0;
-while(i<20){
-    printf("\n hi",i);
+char i=-2;
+do{
+    printf("hi %i \n",i);
     i++;
-};
-
-
-
+}
+while(i<5);
 return 0;
 };
