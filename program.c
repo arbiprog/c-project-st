9ad7f40d8eb42151b68e4c4f41acb0d4e515f216
taha <taharca12@gmail.com>
2022-06-25 21:08:32 +0100

    مثال جاد عن if array

--- program.c
+++ program.c
@@ -1,9 +1,22 @@
-
 #include <stdio.h>
-int main(){
 
-char str[]="بيض صنائعنا سود وقائعنا خضر مرابعنا حمر مواضينا";
-printf("%s",str);
+int main() {
+    int array[] = {1, 7, 4, 5, 9, 3, 5, 11, 6, 3, 4};
+    int i = 0;
 
-return 0;
-};
+    while (i < 10) {
+        /* your code goes here */
+          
+ if (array[6]<5){
+    i++;
+    continue;
+ }else if (array[i]<10){
+    printf("%i \n", array[i]);
+    i++;
+ }else{
+    break;
+ };
+     
+    }
+    return 0;
+}
