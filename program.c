3dc28593e974b8311e0c040923dfc9f59e36ecd7
taha <taharca12@gmail.com>
2022-06-25 12:53:10 +0100

    العمليات الحسابية

--- program.c
+++ program.c
@@ -1,11 +1,38 @@
+// operations with c
+
 #include <stdio.h>
 int main(){
-    const float pi = 3.14;
-    // pi= 4;
-    printf("%f",pi);
+//  عملية result عددين
+// int stn=1;
+// int ndn=2;
+// int result=stn+ndn;
+// printf("%i",result);
+    
 
+// طرح عددين
+// int stn=3;
+// int ndn=2;
+// int result=stn-ndn;
+// printf("%i",result);
+    
+// قسمة عددين
+//     int stn=6;
+// int ndn=2;
+// int result=stn/ndn;
+// printf("%i",result);
+    
 
+// // ناقص واحد
+// int stn=2;
+// int ndn=20;
+// int result=--ndn;
+// printf("%i",result);
+    
 
+زائد واحد
+int stn=2;
+int result=++stn;
+printf("%i",result);
 
 
 
