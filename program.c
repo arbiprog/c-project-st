888507709c7426948067a0a12c23c38e34f7e34d
taha <taharca12@gmail.com>
2022-06-25 14:10:08 +0100

    الجملة الشرطية if , else if, else statments

--- program.c
+++ program.c
@@ -1,19 +1,46 @@
-// logical operations with c
+// if , else if , else statments
 
 #include <stdio.h>
 int main(){
+// int x=3;
+// int y=4;
+// if(x>0 && y>0){
+//     printf("x and y are greater than 0");
+// };
 
-// العطف
-// int x=2;
-// int y=1;
-// printf("%i",y>0 && x>0);
-
-// الفصل
-// int x=0;
-// int y=1;
-// printf("%i",y>0 || x>0);
+// if
+// int x=3;
+// int y=41;
+// if(x>10 || y>10){
+//     printf("x and y are greater than 0");
+// };
 
 
+// if else
+// int x=3;
+// int y=4;
+// if(x>10 && y>10){
+//     printf("x and y are greater than 0");
+// }else{
+//     printf("NOT !");
+// };
 
-    return 0;
+
+// if, else if, else ,
+// int x=3;
+// int y=4;
+// if(x>10 && y>10){
+//     printf("x and y are greater than 10");
+// }else if(x>10 || y>10){
+//     printf("x or y are greater than 10");
+// }else{
+//     printf("no variable is greater than 10 ");
+// };
+
+
+// short hand of f
+int x= 10;
+(x>5) ? printf("x greater than 5") : printf("x not greater than 5");
+
+return 0;
 };
