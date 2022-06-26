73dfc2d02443342a8d3ce86578713239a3c6450a
taha <taharca12@gmail.com>
2022-06-26 15:23:02 +0100

    تمت إعادة التسمية لتسهيل التنفيذ

---
+++ p.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // Create an integer variable that will store the number we get from the user
+
+
+ 
+ printf("what is your name Mr ? \n");
+ char name[5];
+    scanf("%s", &name);
+ printf("your name is %s", name);
+ 
+ 
+ 
+  return 0;
+};
