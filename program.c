2a50b99e6483e1b2cdd2e9e3734a6a9ff5999c78
taha <taharca12@gmail.com>
2022-06-26 15:11:08 +0100

    scanf program example speed

--- program.c
+++ program.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int array[] = {1, 7, 4, 5, 9, 3, 5, 11, 6, 3, 4};
-    int i = 0;
+  // Create an integer variable that will store the number we get from the user
 
-    while (i < 10) {
-        /* your code goes here */
-          
- if (array[6]<5){
-    i++;
-    continue;
- }else if (array[i]<10){
-    printf("%i \n", array[i]);
-    i++;
- }else{
-    break;
- };
-     
-    }
-    return 0;
-}
+ int num;
+ 
+ printf("what is the speed that you want drive with it ? \n");
+ int speed;
+    scanf("%i", &speed);
+ printf("your speed is %i km/h", speed);
+ 
+ 
+ 
+  return 0;
+};
