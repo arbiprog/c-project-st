43c835f3a9a7ea8d54d1e3e1a0cd73da015429a0
taha <taharca12@gmail.com>
2022-06-26 16:12:57 +0100

    sxan f program example pilot

--- p.c
+++ p.c
@@ -8,8 +8,13 @@ int main() {
  printf("what is your name Mr ? \n");
  char name[5];
     scanf("%s", &name);
- printf("your name is %s", name);
+ printf("%s you are the pilot now \n", name);
  
+
+ printf("how fast you want drive ? \n");
+int speed;
+scanf("%i",&speed);
+printf("your speed is : %i km/h",speed);
  
  
   return 0;
